dfc3cf9805be2ee90e0e8f46c8c70fe40e39a192
John17725 <barthjoss@hotmail.es>
2022-03-20 21:21:33 -0600

    Fundamentals and operators

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+// ARRAY
+$Personas = [
+    "Jesus" => 22,
+    "Ricardito" => 2,
+    "Elena" => 22
+];
+
+// var_dump sirve para inspeccionar el contenido de variables y especifica el tipo de dato
+echo"Inspeccionar elementos con var_dump\n\n";
+var_dump( $Personas );
+
+// print_r de igual forma inspecciona el contenido de variables de forma mas limpia y ordenada
+echo"Inspeccionar elementos con print_r\n\n";
+print_r( $Personas );
+
+// Comentarios de codigo
+/*
+    Comentario de codigo
+*/
+
+# Comentario de codigo
+
+
+echo"\n";
+
